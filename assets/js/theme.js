17f393702c270170ccca082630e0971daa846262
Arthur Bondarenko <artur36@inbox.lv>
2021-03-17 15:12:06 +0200

    commit message

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -14,3 +14,43 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+
+
+let btn__play = document.getElementsByClassName("play-btn");
+
+document.querySelector(".play-btn").onclick = function() {
+    this.classList.toggle("hide");
+
+}
+
+
+
+var playButton = document.getElementById("play_button");
+// Event listener for the play/pause button
+playButton.addEventListener("click", function() {
+  if (video.paused == true) {
+    // Play the video
+    video.play();
+
+    // Update the button text to 'Pause'
+    playButton.innerHTML = "Pause";
+  } else {
+    // Pause the video
+    video.pause();
+
+    // Update the button text to 'Play'
+    playButton.innerHTML = "Play";
+  }
+});
+
+
+
+
+
+$(document).ready(function() {
+    $(".header__burger").click(function(event) {
+        $(".header__burger,.nav").toggleClass("active");
+        $("body").toggleClass("lock");
+    }); 
+ });
